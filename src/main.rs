1a013a291d0d3f066632ed3b85818fc469099ae0
Nekuake <ruizale55@gmail.com>
2022-06-18 13:48:11 +0200

    Multiplying

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,16 @@ fn add(list: Vec<&str>){
     println!("{}", result);
 }
 
+fn multiply(list: Vec<&str>){
+    let mut temp_list=list.clone();
+    let mut initial=temp_list[0].parse::<i128>().unwrap();
+    temp_list.remove(0);
+    for number in temp_list{
+        initial=initial*number.parse::<i128>().unwrap();    //We choose this data type since multiplying can result in big numbers. 
+    }
+    println!("{}", initial);
+}
+
 fn minus(list: Vec<&str>){
     let mut temp_list=list.clone();
     let mut initial=temp_list[0].parse::<i32>().unwrap();
@@ -30,6 +40,8 @@ fn main() {
                 add(separated)},
             "minus"=>{
                 minus(separated)},
+            "multiply"=>{
+                 multiply(separated)},
             _=>println!(""),
         }
 
